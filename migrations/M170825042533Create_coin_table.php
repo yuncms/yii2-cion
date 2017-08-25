96b335714fc45e0ed4f1ee43493fd04ba7316d06
Chao Xu <xutongle@gmail.com>
2017-08-25 19:42:22 +0800

    init

---
+++ migrations/M170825042533Create_coin_table.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace yuncms\coin\migrations;
+
+use yii\db\Migration;
+
+/**
+ * Class M170825042533Create_coin_table
+ */
+class M170825042533Create_coin_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%coins}}', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'action' => $this->string(100)->notNull(),
+            'model_id' => $this->integer(),
+            'model_subject' => $this->string(),
+            'coins' => $this->decimal(8, 2)->defaultValue('0.00')->notNull(),
+            'created_at' => $this->integer()->unsigned()->notNull(),
+        ], $tableOptions);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%coins}}');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "M170822042533Create_credits_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
---
+++ migrations/M170825043347Add_coin_field.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace yuncms\coin\migrations;
+
+use yii\db\Migration;
+
+/**
+ * Class M170825043347Add_coin_field
+ */
+class M170825043347Add_coin_field extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%user_extend}}', 'coins', $this->integer()->unsigned()->defaultValue(0)->comment('金币'));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('{{%user_extend}}', 'coins');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "M170822043347Add_credit_field cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
